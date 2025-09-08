ab18bf42eb77c6a5d042a29730ff5f54d870ec88
omkumarbaitha554-collab <omkumarbaitha554@gmail.com>
2025-09-08 21:19:16 +0530

    fix: ensure user name is never empty in users store mutation; fix Next.js hydration error in app layout by suppressing hydration warning

---
+++ app/layout.js
@@ -0,0 +1,35 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+
+const inter = Inter({subsets: ['latin']});
+
+export const metadata = {
+  title: "Splitr",
+  description: "The smartest way to split expenses with friends",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logos/logo-s.png" sizes="any" />
+      </head>
+      <body className={inter.className} suppressHydrationWarning={true}>
+        <ClerkProvider>
+          <ConvexClientProvider>
+            <Header />
+
+          <main className="min-h-screen"> 
+            {children}
+            <Toaster richColors />
+          </main>
+          </ConvexClientProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
